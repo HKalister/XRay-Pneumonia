2feb9e36dcc5064b5381c41246dec79d075ac82e
HKalister <halisterm.udd@gmail.com>
2024-06-17 22:40:40 -0400

    Add files via upload

--- functions/predict.py
+++ functions/predict.py
@@ -6,25 +6,46 @@ import numpy as np
 import os
 from PIL import Image
 import requests
+import zipfile
 
 app = Flask(__name__)
 
-# Función para descargar el modelo desde Google Drive
-def download_model():
-    url = "https://drive.google.com/uc?export=download&id=1DK4L9QSYD4Kz_YiMJLOsNXa7ze2TSt5V"
-    output = "/tmp/best_model_improved.keras"
+# Función para descargar y descomprimir el modelo desde Google Drive
+def download_and_unzip_model():
+    url = "https://drive.google.com/uc?export=download&id=1r4KZYCs_Iyfz2Tml7XkmGAMEbpXSsF7n"
+    local_zip_path = "model.zip"
+    model_filename = "best_model_improved.keras"
+    
     response = requests.get(url)
-    with open(output, 'wb') as file:
+    with open(local_zip_path, 'wb') as file:
         file.write(response.content)
-    return output
+    
+    with zipfile.ZipFile(local_zip_path, 'r') as zip_ref:
+        zip_ref.extractall(".")  # Extraer directamente al directorio actual
+    
+    os.remove(local_zip_path)
+    print(f"Model downloaded and extracted to current directory")
+    return model_filename
 
 # Ruta para cargar el modelo
-model_path = "/tmp/best_model_improved.keras"
+model_path = "best_model_improved.keras"
 if not os.path.exists(model_path):
-    model_path = download_model()
+    print(f"{model_path} not found. Downloading and extracting model...")
+    model_path = download_and_unzip_model()
 
-# Cargar el modelo entrenado
-model = load_model(model_path)
+# Verificar la existencia del archivo y su tamaño antes de cargarlo
+if os.path.exists(model_path):
+    print(f"Loading model from {model_path}")
+    file_size = os.path.getsize(model_path)
+    print(f"File size: {file_size} bytes")
+
+    try:
+        model = load_model(model_path)
+    except ValueError as e:
+        print(f"Error loading model: {e}")
+        raise
+else:
+    raise FileNotFoundError(f"Model file not found at {model_path}")
 
 @app.route('/')
 def upload_form():
@@ -41,7 +62,8 @@ def predict():
         return jsonify({"error": "No selected file"})
 
     if file:
-        filepath = os.path.join("/tmp", file.filename)
+        filepath = os.path.join("uploads", file.filename)
+        os.makedirs(os.path.dirname(filepath), exist_ok=True)
         file.save(filepath)
 
         img = image.load_img(filepath, target_size=(150, 150), color_mode='grayscale')
